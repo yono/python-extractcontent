c639e74af8f0ac05aee421cdbbfa1c057ea35652
yono <yono.05@gmail.com>
2010-05-09 12:58:03 +0900

    refactoring with pep8

--- extractcontent.py
+++ extractcontent.py
@@ -3,30 +3,40 @@
 import re
 import unicodedata
 
+
 class ExtractContent(object):
 
     # convert character to entity references
     CHARREF = {
-        "nbsp" :" ",
-        "lt"   :"<",
-        "gt"   :"<",
-        "amp"  :"&",
-        "laquo":u"\xc2\xab",
-        "raquo":u"\xc2\xbb",
-    }
-    
-    # Default option parameters.
-    default = {
-        "threshold"          : 100,                                                  # threshold for score of the text
-        "min_length"         : 80,                                                   # minimum length of evaluated blocks
-        "decay_factor"       : 0.73,                                                 # decay factor for block score
-        "continuous_factor"  : 1.62,                                                 # continuous factor for block score ( the larger, the harder to continue )
-        "punctuation_weight" : 10,                                                   # score weight for punctuations
-        "punctuations"       : r"(?is)(\343\200[\201\202]|\357\274[\201\214\216\237]|\.[^A-Za-z0-9]|,[^0-9]|!|\?)", # punctuation characters
-        "waste_expressions"  : r"(?i)Copyright|All Rights Reserved",                 # characteristic keywords including footer
-        "debug"              : False,                                                # if true, output block information to stdout
+        "nbsp": " ",
+        "lt": "<",
+        "gt": "<",
+        "amp": "&",
+        "laquo": u"\xc2\xab",
+        "raquo": u"\xc2\xbb",
     }
 
+    # Default option parameters.
+    default = {
+        "threshold": 100,
+        # threshold for score of the text
+        "min_length": 80,
+        # minimum length of evaluated blocks
+        "decay_factor": 0.73,
+        # decay factor for block score
+        "continuous_factor": 1.62,
+        # continuous factor for block score
+        #( the larger, the harder to continue )
+        "punctuation_weight": 10,
+        # score weight for punctuations
+        "punctuations": r"""(?is)(\343\200[\201\202]|\357\274
+        [\201\214\216\237]|\.[^A-Za-z0-9]|,[^0-9]|!|\?)""",
+        # punctuation characters
+        "waste_expressions": r"(?i)Copyright|All Rights Reserved",
+        # characteristic keywords including footer
+        "debug": False,
+        # if true, output block information to stdout
+    }
 
     def __init__(self, opt=None):
         if opt != None:
@@ -42,7 +52,8 @@ class ExtractContent(object):
     # Analyses the given HTML text, extracts body and title.
     def analyse(self, html, opt=None):
         # flameset or redirect
-        if re.search(r"(?i)<\/frameset>|<meta\s+http-equiv\s*=\s*[\"']?refresh['\"]?[^>]*url",html) != None:
+        if re.search(r"""(?i)<\/frameset>|<meta\s+http-equiv\s*=\s*
+                [\"']?refresh['\"]?[^>]*url""", html) != None:
             return ["", self.extract_title(html)]
 
         # option parameters
@@ -61,9 +72,13 @@ class ExtractContent(object):
             title = self.extract_title(html)
 
         # Google AdSense Section Target
-        html = re.sub(r"(?is)<!--\s*google_ad_section_start\(weight=ignore\)\s*-->.*?<!--\s*google_ad_section_end.*?-->","", html)
-        if re.search(r"(?is)<!--\s*google_ad_section_start[^>]*-->",html) != None:
-            result = re.findall(r"(?is)<!--\s*google_ad_section_start[^>]*-->.*?<!--\s*google_ad_section_end.*?-->",html)
+        html = re.sub(r"""(?is)<!--\s*google_ad_section_start\(weight=
+                ignore\)\s*-->.*?<!--\s*google_ad_section_end.*?-->""",
+                "", html)
+        if re.search(r"(?is)<!--\s*google_ad_section_start[^>]*-->",
+                html) != None:
+            result = re.findall(r"""(?is)<!--\s*google_ad_section_start
+                    [^>]*-->.*?<!--\s*google_ad_section_end.*?-->""", html)
             html = "\n".join(result)
 
         # eliminate useless text
@@ -71,7 +86,8 @@ class ExtractContent(object):
 
         # heading tags including title
         self.title = title
-        html = re.sub(r"(?s)(<h\d\s*>\s*(.*?)\s*</h\d\s*>)", self._estimate_title, html)
+        html = re.sub(r"(?s)(<h\d\s*>\s*(.*?)\s*</h\d\s*>)",
+                self._estimate_title, html)
 
         # extract text blocks
         factor = 1.0
@@ -79,7 +95,9 @@ class ExtractContent(object):
         body = ''
         score = 0
         bodylist = []
-        list = re.split(r"</?(?:div|center|td)[^>]*>|<p\s*[^>]*class\s*=\s*[\"']?(?:posted|plugin-\w+)['\"]?[^>]*>",html)
+        list =
+        re.split(r"""</?(?:div|center|td)[^>]*>|<p\s*[^>]*class\s*=\s*
+                [\"']?(?:posted|plugin-\w+)['\"]?[^>]*>""", html)
         for block in list:
             if self._has_only_tags(block):
                 continue
@@ -93,33 +111,35 @@ class ExtractContent(object):
                 continue
 
             # calculate score of block
-            c = (len(notlinked) + self._count_pattern(notlinked, opt["punctuations"]) \
-                    * opt["punctuation_weight"]) * factor
+            c = (len(notlinked) + self._count_pattern(notlinked,
+                opt["punctuations"]) * opt["punctuation_weight"]) * factor
             factor *= opt["decay_factor"]
-            not_body_rate = self._count_pattern(block,opt["waste_expressions"]) \
-                    + self._count_pattern(block,r"amazon[a-z0-9\.\/\-\?&]+-22") / 2.0
+            not_body_rate = self._count_pattern(block,
+                opt["waste_expressions"]) + self._count_pattern(block,
+                        r"amazon[a-z0-9\.\/\-\?&]+-22") / 2.0
             if not_body_rate > 0:
-                c *= (0.72 ** not_body_rate) 
+                c *= (0.72 ** not_body_rate)
             c1 = c * continuous
             if opt["debug"]:
-                print "----- %f*%f=%f %d \n%s" % (c, continuous, c1, len(notlinked), self._strip_tags(block)[0:100])
+                print "----- %f*%f=%f %d \n%s" %\
+                    (c, continuous, c1, len(notlinked),
+                            self._strip_tags(block)[0:100])
 
             # tread continuous blocks as cluster
             if c1 > opt["threshold"]:
                 body += block + "\n"
                 score += c1
                 continuous = opt["continuous_factor"]
-            elif c > opt["threshold"]: # continuous block end
+            elif c > opt["threshold"]:  # continuous block end
                 bodylist.append((body, score))
                 body = block + "\n"
                 score = c
                 continuous = opt["continuous_factor"]
-        
+
         bodylist.append((body, score))
-        body = reduce(lambda x,y:x if x[1]>=y[1] else y, bodylist)
-        #return (self._strip_tags(body[0]), title)
+        body = reduce(lambda x, y: x if x[1] >= y[1] else y, bodylist)
         self.body = body[0]
-    
+
     def as_html(self):
         return (self.body, self.title)
 
@@ -127,8 +147,8 @@ class ExtractContent(object):
         return (self._strip_tags(self.body), self.title)
 
     # Extract title.
-    def extract_title(self,st):
-        result = re.search(r"(?s)<title[^>]*>\s*(.*?)\s*</title\s*>",st)
+    def extract_title(self, st):
+        result = re.search(r"(?s)<title[^>]*>\s*(.*?)\s*</title\s*>", st)
         if result != None:
             return self._strip_tags(result.group(0))
         else:
@@ -150,47 +170,52 @@ class ExtractContent(object):
         else:
             return match.group(1)
 
-
     # Eliminates useless tags
     def _eliminate_useless_tags(self, html):
         # Eliminate useless symbols
         html = html.encode('utf-8')
-        html = re.sub(r"\342(?:\200[\230-\235]|\206[\220-\223]|\226[\240-\275]|\227[\206-\257]|\230[\205\206])","", html)
+        html = re.sub(r"""\342(?:\200[\230-\235]|\206[\220-\223]|
+                \226[\240-\275]|\227[\206-\257]|\230[\205\206])""",
+                "", html)
         html = html.decode('utf-8')
         # Eliminate useless html tags
-        html = re.sub(r"(?is)<(script|style|select|noscript)[^>]*>.*?</\1\s*>","", html)
-        html = re.sub(r"(?s)<!--.*?-->","", html)
-        html = re.sub(r"<![A-Za-z].*?>/s","", html)
-        html = re.sub(r"(?s)<div\s[^>]*class\s*=\s*['\"]?alpslab-slide[\"']?[^>]*>.*?</div\s*>","", html)
-        html = re.sub(r"(?is)<div\s[^>]*(id|class)\s*=\s*['\"]?\S*more\S*[\"']?[^>]*>","", html)
+        html =
+        re.sub(r"""(?is)<(script|style|select|noscript)[^>]*>.*?</\1\s*>""",
+                "", html)
+        html = re.sub(r"(?s)<!--.*?-->", "", html)
+        html = re.sub(r"<![A-Za-z].*?>/s", "", html)
+        html = re.sub(r"""(?s)<div\s[^>]*class\s*=\s*['\"]?alpslab-slide
+                [\"']?[^>]*>.*?</div\s*>""", "", html)
+        html = re.sub(r"""(?is)<div\s[^>]*(id|class)\s*=\s*['\"]
+                ?\S*more\S*[\"']?[^>]*>""", "", html)
         return html
 
     # Checks if the given block has only tags without text.
     def _has_only_tags(self, st):
-        st = re.sub(r"(?is)<[^>]*>","", st)
-        st = re.sub(r"&nbsp;","", st)
+        st = re.sub(r"(?is)<[^>]*>", "", st)
+        st = re.sub(r"&nbsp;", "", st)
         st = st.strip()
         return len(st) == 0
 
     # eliminate link tags
     def _eliminate_link(self, html):
         count = 0
-        notlinked, count = re.subn(r"(?is)<a\s[^>]*>.*?<\/a\s*>","", html)
-        notlinked = re.sub(r"(?is)<form\s[^>]*>.*?</form\s*>","", notlinked)
-        notlinked = self._strip_tags(notlinked) 
-        if (len(notlinked) < 20*count) or (self._islinklist(html)):
+        notlinked, count = re.subn(r"(?is)<a\s[^>]*>.*?<\/a\s*>", "", html)
+        notlinked = re.sub(r"(?is)<form\s[^>]*>.*?</form\s*>", "", notlinked)
+        notlinked = self._strip_tags(notlinked)
+        if (len(notlinked) < 20 * count) or (self._islinklist(html)):
             return ""
         return notlinked
 
     # determines whether a block is link list or not
     def _islinklist(self, st):
-        result = re.search(r"(?is)<(?:ul|dl|ol)(.+?)</(?:ul|dl|ol)>",st)
+        result = re.search(r"(?is)<(?:ul|dl|ol)(.+?)</(?:ul|dl|ol)>", st)
         if result != None:
             listpart = result.group(1)
-            outside = re.sub(r"(?is)<(?:ul|dl)(.+?)</(?:ul|dl)>","", st)
-            outside = re.sub(r"(?is)<.+?>","", outside)
-            outside = re.sub(r"\s+","", outside)
-            list = re.split(r"<li[^>]*>",listpart)
+            outside = re.sub(r"(?is)<(?:ul|dl)(.+?)</(?:ul|dl)>", "", st)
+            outside = re.sub(r"(?is)<.+?>", "", outside)
+            outside = re.sub(r"\s+", "", outside)
+            list = re.split(r"<li[^>]*>", listpart)
             rate = self._evaluate_list(list)
             return len(outside) <= len(st) / (45 / rate)
         return False
@@ -213,8 +238,9 @@ class ExtractContent(object):
         if type(st) != str:
             st = unicodedata.normalize("NFKC", st)
             st = st.encode('utf-8')
-        st = re.sub(r'\342[\224\225][\200-\277]','',st) # keisen
-        st = re.sub(r"&(.*?);",lambda x:self.CHARREF.get(x.group(1), x.group()), st)
+        st = re.sub(r'\342[\224\225][\200-\277]', '', st)  # keisen
+        st = re.sub(r"&(.*?);", lambda x: self.CHARREF.get(x.group(1),
+            x.group()), st)
         st = st.decode('utf-8')
         st = re.sub(r"[ \t]+", " ", st)
         st = re.sub(r"\n\s*", "\n", st)
